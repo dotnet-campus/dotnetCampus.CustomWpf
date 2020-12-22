3397e4c99913a92b54fdc072cc7effda23bf27c4
lindexi <lindexi_gd@163.com>
2020-12-22 19:32:10 +0800

    添加签名

---
+++ src/Microsoft.DotNet.Wpf/src/dotnetCampus.WPF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+namespace dotnetCampus.WPF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
