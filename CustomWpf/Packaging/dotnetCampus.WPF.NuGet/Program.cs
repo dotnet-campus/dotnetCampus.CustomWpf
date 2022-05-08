74f19b69e1104eaee1e596f2f2f55518cc13927f
lindexi <lindexi_gd@163.com>
2022-05-08 17:38:52 +0800

    加上一些垃圾代码

--- CustomWpf/Packaging/dotnetCampus.WPF.NuGet/Program.cs
+++ CustomWpf/Packaging/dotnetCampus.WPF.NuGet/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using dotnetCampus.Configurations;
+
+namespace dotnetCampus.WPF.NuGet;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        MainTask mainTask = new MainTask();
+        mainTask.Run();
+    }
+}
