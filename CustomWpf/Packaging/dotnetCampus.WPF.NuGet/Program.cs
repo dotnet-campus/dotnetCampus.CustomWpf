59affa6d8179807cad3f24bae793aec44ce76778
lindexi <lindexi_gd@163.com>
2022-05-08 20:29:27 +0800

    加上注释

--- CustomWpf/Packaging/dotnetCampus.WPF.NuGet/Program.cs
+++ CustomWpf/Packaging/dotnetCampus.WPF.NuGet/Program.cs
@@ -9,6 +9,8 @@ using dotnetCampus.DotNETBuild.Utils;
 
 namespace dotnetCampus.WPF.NuGet;
 
+// 这个项目现在完全没有被用上
+// 不删除，后续需要再发布 x64 版本
 internal class Program
 {
     public static void Main(string[] args)
